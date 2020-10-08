45982de1ae2061c414ecade608c0715fc45d7ff4
Chengze Wu <wuc23@student.douglascollege.ca>
2020-10-07 17:23:04 -0700

    Added group description

---
+++ src/main/java/com/csis3275/model_cwu_18/GroupDesciption_cwu_18.java
@@ -0,0 +1,14 @@
+package com.csis3275.model_cwu_18;
+
+public class GroupDesciption_cwu_18 {
+	private String groupDescription;
+
+	public String getGroupDescription() {
+		return groupDescription;
+	}
+
+	public void setGroupDescription(String groupDescription) {
+		this.groupDescription = groupDescription;
+	}
+
+}
