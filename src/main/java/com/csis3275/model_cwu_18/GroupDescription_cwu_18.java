1b4933a17bf25ec439adc3e21466d267fcadd43a
Chengze Wu <wuc23@student.douglascollege.ca>
2020-10-07 18:17:06 -0700

    solve name tyoi

--- src/main/java/com/csis3275/model_cwu_18/GroupDescription_cwu_18.java
+++ src/main/java/com/csis3275/model_cwu_18/GroupDescription_cwu_18.java
@@ -1,6 +1,6 @@
 package com.csis3275.model_cwu_18;
 
-public class GroupDesciption_cwu_18 {
+public class GroupDescription_cwu_18 {
 	private String groupDescription;
 
 	public String getGroupDescription() {
