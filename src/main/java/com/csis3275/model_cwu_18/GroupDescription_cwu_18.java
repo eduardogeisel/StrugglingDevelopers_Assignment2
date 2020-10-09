b910a666ed794f70afda5c3898a5a4a689c39722
Soheila Hoseini <hoseinis@student.douglascollege.ca>
2020-10-08 21:40:12 -0700

    Added Soheila's member to the class

--- src/main/java/com/csis3275/model_cwu_18/GroupDescription_cwu_18.java
+++ src/main/java/com/csis3275/model_cwu_18/GroupDescription_cwu_18.java
@@ -7,11 +7,13 @@ public class GroupDescription_cwu_18 {
 	GroupMember_cwu_18 member1 = new GroupMember_cwu_18();
 	GroupMember_epe_07 member2 = new GroupMember_epe_07();
 	GroupMember_mjo_56 member3 = new GroupMember_mjo_56();
+	GroupMember_sho_38 member4 = new GroupMember_sho_38();
+
 
 	public String getGroupDescription() {
 		
 		return "This is a wonderful group which includes 4 students.<br>"
-				+ member1.getName() + " (Travis)" + ", " + member2.getName() + ", " + member3.getName() + " and Soheila. <br>"
+				+ member1.getName() + " (Travis)" + ", " + member2.getName() + ", " + member3.getName() + " and "+member4.getName()+ " <br>"
 				+ "We teamed up to work on the Software engineering project.<br>"
 				+ "Our project is about Room Booking system. <br>";
 	}
