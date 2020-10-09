c3d5a85ea54c805e84ed27c3a08a753da4e3410b
Soheila Hoseini <hoseinis@student.douglascollege.ca>
2020-10-08 21:36:57 -0700

    Created Soheila's group member class

---
+++ src/main/java/com/csis3275/model_cwu_18/GroupMember_sho_38.java
@@ -0,0 +1,18 @@
+package com.csis3275.model_cwu_18;
+
+public class GroupMember_sho_38 {
+	private String name;
+	private String id;
+	private String imgSrc;
+	
+	public String getName() {
+		return "Soheila Hoseini";
+	}
+	public String getId() {
+		return "300311938";
+	}
+
+	public String getImgSrc() {
+		return "selfie_sho_38.png";
+	}
+}
