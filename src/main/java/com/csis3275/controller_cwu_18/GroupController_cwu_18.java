3fe47278ce918b0e37bed9f2fdb3a96db2f5c9eb
Soheila Hoseini <hoseinis@student.douglascollege.ca>
2020-10-08 21:42:58 -0700

    Added Soheila to the controller'

--- src/main/java/com/csis3275/controller_cwu_18/GroupController_cwu_18.java
+++ src/main/java/com/csis3275/controller_cwu_18/GroupController_cwu_18.java
@@ -18,6 +18,7 @@ import com.csis3275.model_cwu_18.GroupDescription_cwu_18;
 import com.csis3275.model_cwu_18.GroupMember_cwu_18;
 import com.csis3275.model_cwu_18.GroupMember_epe_07;
 import com.csis3275.model_cwu_18.GroupMember_mjo_56;
+import com.csis3275.model_cwu_18.GroupMember_sho_38;
 
 @Controller
 //Here we will have this controller respond to requests from /employee for the employee servlet.
@@ -33,10 +34,13 @@ public class GroupController_cwu_18 {
 		GroupMember_cwu_18 member1 = new GroupMember_cwu_18();
 		GroupMember_epe_07 member2 = new GroupMember_epe_07();
 		GroupMember_mjo_56 member3 = new GroupMember_mjo_56();
+		GroupMember_sho_38 member4 = new GroupMember_sho_38();
+
 		model.addAttribute("group", group);
 		model.addAttribute("member1", member1);
 		model.addAttribute("member2", member2);
 		model.addAttribute("member3", member3);
+		model.addAttribute("member4", member4);
 		return "group_cwu_18";
 	}
 
