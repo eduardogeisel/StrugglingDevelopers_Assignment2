7d79932d654beeb9d98a5e83fc5f2e3a7d1ff33a
Eduardo Geisel Peres <eduardogeisel@hotmail.com>
2020-10-08 17:35:26 -0700

    Added grouGroupMember_epe_07.java to project.

---
+++ src/main/java/com/csis3275/model_cwu_18/GroupMember_epe_07.java
@@ -0,0 +1,20 @@
+package com.csis3275.model_cwu_18;
+
+public class GroupMember_epe_07 {
+	
+	private String name;
+	private String id;
+	private String imgSrc;
+	
+	public String getName() {
+		return "Eduardo Peres";
+	}
+	public String getId() {
+		return "300318007";
+	}
+
+	public String getImgSrc() {
+		return "selfie_epe_07.jpg";
+	}
+	
+}
