4d4a889783a069067e6a0a7bb8e9e02a92d9ef55
Eduardo Geisel Peres <eduardogeisel@hotmail.com>
2020-10-08 20:35:31 -0700

    Changed group description with better coding standards

--- src/main/java/com/csis3275/model_cwu_18/GroupDescription_cwu_18.java
+++ src/main/java/com/csis3275/model_cwu_18/GroupDescription_cwu_18.java
@@ -3,12 +3,17 @@ package com.csis3275.model_cwu_18;
 public class GroupDescription_cwu_18 {
 	private String teamName;
 	private String groupDescription;
+	
+	GroupMember_cwu_18 member1 = new GroupMember_cwu_18();
+	GroupMember_epe_07 member2 = new GroupMember_epe_07();
+	GroupMember_mjo_56 member3 = new GroupMember_mjo_56();
 
 	public String getGroupDescription() {
+		
 		return "This is a wonderful group which includes 4 students.<br>"
-				+ "Eduardo, Soheila, Mansi, and Travis. <br>"
-				+ "We team up to finish Software engineering project.<br>"
-				+ "Our project is about booking room system. <br>";
+				+ member1.getName() + " (Travis)" + ", " + member2.getName() + ", " + member3.getName() + " and Soheila. <br>"
+				+ "We teamed up to work on the Software engineering project.<br>"
+				+ "Our project is about Room Booking system. <br>";
 	}
 
 
