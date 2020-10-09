209b9f02da1c69724639b8979600af0dbfac4e66
Soheila Hoseini <hoseinis@student.douglascollege.ca>
2020-10-08 22:29:57 -0700

    Added javadoc for Soheila

--- src/main/java/com/csis3275/model_cwu_18/GroupMember_sho_38.java
+++ src/main/java/com/csis3275/model_cwu_18/GroupMember_sho_38.java
@@ -1,10 +1,19 @@
 package com.csis3275.model_cwu_18;
 
+/**
+ *  This is a class for Soheila 
+ * @author 300311938
+ *
+ */
 public class GroupMember_sho_38 {
 	private String name;
 	private String id;
 	private String imgSrc;
-	
+
+	/**
+	 * These are getter methods
+	 * @return
+	 */
 	public String getName() {
 		return "Soheila Hoseini";
 	}
